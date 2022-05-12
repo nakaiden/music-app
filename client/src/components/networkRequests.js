a0cf7825792e70b9148e9078ae421834156d280f
nakaiden <nakaiden@gmail.com>
2022-05-11 20:33:22 -0400

    api update

--- client/src/components/networkRequests.js
+++ client/src/components/networkRequests.js
@@ -1,4 +1,5 @@
-export const api = "http://localhost:3030";
+//export const api = "http://localhost:3030";
+export const api = "https://djkcvyzo251au.cloudfront.net";
 
 export const getAllSongs = async () => {
     const songs = await fetch(`${api}/songs`);
